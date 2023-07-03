677ae7b46f6e3376cda13c839d6e3f8d5d85953f
kittipol <kittipol.surapinchai@gmail.com>
2023-07-03 15:04:40 +0700

    Netflix clone

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -53,7 +53,7 @@ const Auth = () => {
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login'? 'First time using Netflix?' : 'Already have an account'}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                            {variant === 'login'? 'Create an account': "login"}
+                            {variant === 'login'? 'Create an account': "login"} 
                             </span>
                         </p>
                     </div>
